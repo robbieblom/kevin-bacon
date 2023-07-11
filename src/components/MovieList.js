3bbb036c4b843d5026a0669b28ff01e26396a511
Robbie Blom <robbie.blom@bytetheoryinnovations.com>
2023-07-11 14:21:21 -0400

    fixed responsiveness of Results section for the grid.

--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -1,5 +1,5 @@
 import { ByteTheoryTheme } from "@bytetheoryinnovations/bytetheory-ui-library";
-import { Paper, Stack, Typography, useTheme } from "@mui/material";
+import { Paper, Typography, useTheme } from "@mui/material";
 import Grid2 from "@mui/material/Unstable_Grid2/Grid2";
 import React from "react";
 import { _hexToRgb } from "../utils/utils";
@@ -18,34 +18,28 @@ export const MovieList = ({ actor, movies }) => {
           elevation={5}
           sx={{
             backgroundColor: `rgba(${r}, ${g}, ${b}, 0.1)`,
+            padding: "35px",
           }}
         >
-          <Stack
-            direction={"column"}
-            spacing={2}
-            sx={{ padding: "30px", height: "100%" }}
-            flexWrap={"wrap"}
-          >
-            <Grid2 container alignItems={"center"} spacing={4}>
-              {movies.length > 0 ? (
-                movies.map((movie, index) => (
-                  <Grid2 key={index}>
-                    <MovieCard
-                      actor={actor}
-                      movieDetails={movie.movieDetails}
-                      collaborator={movie.targetAsCastMember}
-                    />
-                  </Grid2>
-                ))
-              ) : (
-                <Grid2 flexGrow={1} sx={{ height: "100%" }}>
-                  <Typography variant="body1" textAlign={"center"}>
-                    No collaborations
-                  </Typography>
+          <Grid2 container justifyContent="space-evenly" spacing={4}>
+            {movies.length > 0 ? (
+              movies.map((movie, index) => (
+                <Grid2 key={index}>
+                  <MovieCard
+                    actor={actor}
+                    movieDetails={movie.movieDetails}
+                    collaborator={movie.targetAsCastMember}
+                  />
                 </Grid2>
-              )}
-            </Grid2>
-          </Stack>
+              ))
+            ) : (
+              <Grid2 flexGrow={1} sx={{ height: "100%" }}>
+                <Typography variant="body1" textAlign={"center"}>
+                  No collaborations
+                </Typography>
+              </Grid2>
+            )}
+          </Grid2>
         </Paper>
       </ByteTheoryTheme>
     </>
